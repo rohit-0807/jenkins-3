145804a757f53ff816ff36daef88aec9aadccf8a
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-05-14 09:47:04 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo {
+    public static void main(String[] args) {
+        int n = 10; 
+        int m = 5; 
+        int add = n + m;
+        System.out.println("Sum of " + n + " and " + m + " is: " + add);
+    }    
+}
